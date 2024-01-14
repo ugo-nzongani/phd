42b2e67a26b298c08b4829930bdd5f8063f2d24a
Ugo Nzongani <60008078+ugo-nzongani@users.noreply.github.com>
2024-01-15 00:36:49 +0100

    Update fujiwara.py

--- quantum_walks/shift_operators/fujiwara.py
+++ quantum_walks/shift_operators/fujiwara.py
@@ -1,1 +1,55 @@
-d
+from qiskit import *
+from numpy import pi
+
+def shift_gates(n):
+    """
+    Parameters
+    ----------
+    n : int
+        The number of qubits encoding the position
+
+    Returns
+    -------
+    qiskit.circuit.gate.Gate,qiskit.circuit.gate.Gate
+        Quantum gates Z+ and Z- used to move the walker
+    """
+    # Position register
+    b = QuantumRegister(n, name="b")
+    # Coin register
+    s = QuantumRegister(1, name="s")
+    qc = QuantumCircuit(b, s)
+    for i in range(n):
+        ctrl = [s[0]] + b[:n-i-1]
+        qc.mct(ctrl, [b[n-i-1]])
+    z_up = qc.to_gate(label="Z+")
+    z_down = qc.inverse().to_gate(label="Z-")
+    return z_up, z_down
+
+def fujiwara_shift(n):
+    """
+    Parameters
+    ----------
+    n : int
+        The number of qubits encoding the position
+
+    Returns
+    -------
+    qiskit.circuit.quantumcircuit.QuantumCircuit
+        Quantum circuit implementing the shift operator introduced by Fujiwara et al. (10.1103/PhysRevA.72.032329)
+    """
+    # Position register
+    b = QuantumRegister(n, name="b")
+    # Coin register
+    s = QuantumRegister(1, name="s")
+    qc = QuantumCircuit(b, s)
+    qubits = [i for i in b] + [s[0]]
+    z_up, z_down = shift_gates(n)
+    qc.append(z_up, qubits)
+    #qc.barrier()
+    qc.x(s[0])
+    #qc.barrier()
+    qc.append(z_down, qubits)
+    #qc.barrier()
+    qc.x(s[0])
+    #qc.barrier()
+    return qc
