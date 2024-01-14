c3c9eb8542cf9db488fc7de336b59c6fa3b37012
Ugo Nzongani <60008078+ugo-nzongani@users.noreply.github.com>
2024-01-15 00:45:05 +0100

    Update and rename shakeel_qft to shakeel_qft.py

---
+++ quantum_walks/shift_operators/shakeel_qft.py
@@ -0,0 +1,35 @@
+from qiskit import *
+from qiskit.circuit.library import QFT
+from numpy import pi
+
+def shakeel_shift(n):
+    """
+    Parameters
+    ----------
+    n : int
+        The number of qubits encoding the position
+
+    Returns
+    -------
+    qiskit.circuit.quantumcircuit.QuantumCircuit
+        Quantum circuit implementing the shift operator using the QFT introduced by Asif Shakeel (10.1007/s11128-020-02834-y)
+    """
+    # Position register
+    b = QuantumRegister(n, name="b")
+    # Coin register
+    s = QuantumRegister(1, name="s")
+    # Create a quantum circuit using the quantum and classical registers
+    qc = QuantumCircuit(b, s)
+    position_qubits = qc.qubits[:n]
+    qft = QFT(n, do_swaps=False)
+    qc.x(s[0])
+    for i in range(n):
+        qc.cx(s[0], b[n-i-1])
+    qc.append(qft, position_qubits)
+    for i in range(n):
+        qc.p(2*pi/2**(n-i), n-i-1)
+    qc.append(qft.inverse(), position_qubits)
+    for i in range(n):
+        qc.cx(s[0], b[i])
+    qc.x(s[0])
+    return qc
